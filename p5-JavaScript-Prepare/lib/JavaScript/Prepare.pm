3eda5c61f8356e6b4cbf9be38bb5d766e1cf4fab
Mark Norman Francis <norm@cackhanded.net>
2011-01-22 12:41:14 +0000

    Test minifying directories.

--- p5-JavaScript-Prepare/lib/JavaScript/Prepare.pm
+++ p5-JavaScript-Prepare/lib/JavaScript/Prepare.pm
@@ -4,6 +4,7 @@ use 5.010;
 use strict;
 use warnings;
 
+use FileHandle;
 use JavaScript::Minifier::XS    qw( minify );
 
 
@@ -26,6 +27,76 @@ sub process_string {
     return "${minified}\n"
         if defined $minified && length $minified;
     return '';
+}
+
+sub process_file {
+    my $self = shift;
+    my $file = shift;
+    
+    my $content = $self->read_file( $file );
+    return '' unless $content;
+    
+    return $self->process_string( $content );
+}
+sub read_file {
+    my $self = shift;
+    my $file = shift;
+    
+    my $handle = FileHandle->new( $file )
+        or return;
+    
+    my $content = do {
+        local $/;
+        <$handle>
+    };
+    
+    return $content;
+}
+
+sub process_directory {
+    my $self      = shift;
+    my $directory = shift;
+    
+    my @files = $self->get_files_in_directory( $directory );
+    my $minified;
+    
+    foreach my $file ( @files ) {
+        $minified .= $self->process_file( $file );
+    }
+    
+    return $minified;
+}
+
+sub get_files_in_directory {
+    my $self      = shift;
+    my $directory = shift;
+    
+    opendir my $handle, $directory
+        or return;
+    
+    my @files;
+    my @directories;
+    while ( my $entry = readdir $handle ) {
+        next if $entry =~ m{^\.};
+        
+        my $target = "$directory/$entry";
+        
+        push( @files, $target ) if -f $target;
+        push( @directories, $target ) if -d $target;
+    }
+    closedir $handle;
+    
+    foreach my $dir ( @directories ) {
+        my @subfiles;
+        
+        foreach my $file ( $self->get_files_in_directory( $dir ) ) {
+            push @subfiles, $file;
+        }
+        
+        @files = ( @subfiles, @files );
+    }
+    
+    return @files;
 }
 
 1;
